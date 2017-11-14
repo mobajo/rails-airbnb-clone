2d797c999f491b9bb5f7deea5863fbf4f01e221c
Niels Møller-Hansen <moeller84@gmail.com>
2017-11-14 16:19:30 +0100

    Spaceships controller & views done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :users do 
+  resources :spaceships, only: [ :new, :create, :index ] 
+  end
+
+  resources :spaceships, only: [ :show, :edit, :update, :destroy ]
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
