b8f37cec6c0c00bc5cc21f912d52bf4f5d3128ac
Morten Jorgensen <morten00@gmail.com>
2017-11-14 18:42:02 +0100

    Created seed file and implemented Cloudinary/attachinary for photos

--- app/models/spaceship.rb
+++ app/models/spaceship.rb
@@ -2,9 +2,13 @@ class Spaceship < ApplicationRecord
   belongs_to :user
   # has_many :users, through: :bookings
   has_many :bookings
+  has_attachments :photos, maximum: 4
 
   # validates :name, uniqueness: true, presence: true
   # validates :address, presence: true
   # validates :price, presence: true, numericality: { only_integer: true }
   # validates :description, presence: true
+  def product_params
+    params.require(:product).permit(:name, :address, :price, :description, :speed, :weaponry, photos: [])
+  end
 end
